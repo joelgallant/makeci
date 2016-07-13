6f676e01a008d564e1355b8463c723ad40438e90
Joel Gallant <joelgallant236@gmail.com>
2016-07-13 16:40:39 -0600

    Unlock at end

--- apt-wait.py
+++ apt-wait.py
@@ -3,6 +3,8 @@ handle = open("/var/lib/dpkg/lock", "w")
 while True:
     try:
         fcntl.lockf(handle, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        exit(0)
+        break
     except IOError:
         time.sleep(1)
+
+fcntl.lockf(handle, fcntl.LOCK_UN)
